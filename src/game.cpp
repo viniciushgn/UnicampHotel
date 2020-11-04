bf286738daae1c75c4a1b729cc4a56b18c1254b0
vinihg <v188182@dac.unicamp.br>
2020-11-04 10:05:05 -0300

    Build falha, imagem não aparece

--- src/game.cpp
+++ src/game.cpp
@@ -11,22 +11,23 @@ private:
 int posX,posY;//posicao do canto inferior esquerdo do sprite.
 int sizeX,sizeY;
 int colliderX, colliderY;//collider a partir do canto inferior esquerdo do sprite
-SDL_Texture* sprite;
-std::string spritePath;
+std::string spritePath;//caminho para o arquivo da sprite
 
 public:
-Objeto(int px, int py, int sx, int sy, int colx, int coly, std::string sp, SDL_Renderer* renderer1);
+Objeto(int px, int py, int sx, int sy, int colx, int coly, std::string sp);
 int getPosX();
 int getPosY();
 int getSizeX();
 int getSizeY();
 int getColX();
 int getColY();
-SDL_Texture* getSprite();
+std::string getSpritePath();
 
+void addPos(int nx, int ny);
+void subPos(int nx, int ny);
 };
 
-Objeto::Objeto(int px, int py, int sx, int sy, int colx, int coly, std::string sp, SDL_Renderer* renderer1){
+Objeto::Objeto(int px, int py, int sx, int sy, int colx, int coly, std::string sp){
 this->posX = px;
 this->posY = py;
 this->sizeX = sx;
@@ -34,7 +35,6 @@ this->sizeY = sy;
 this->colliderX = colx;
 this->colliderY = coly;
 this->spritePath = sp;//caminho para o arquivo de imagem da sprite
-this->sprite = IMG_LoadTexture(renderer1, sp.c_str());//endereco da textura
 }
 int Objeto::getPosX(){
   return this->posX;
@@ -54,29 +54,36 @@ int Objeto::getColX(){
 int Objeto::getColY(){
   return this->colliderY;
 }
-SDL_Texture* Objeto::getSprite(){
-  return this->sprite;
+std::string Objeto::getSpritePath(){
+  return this->spritePath;
 }
+void Objeto::addPos(int nx, int ny){
+  this->posX = this->posX + nx;
+  this->posY = this->posY + ny;
+}
+
+void Objeto::subPos(int nx, int ny){
+  this->posX = this->posX - nx;
+  this->posY = this->posY - ny;
+}
+
+
 //------------------------------------------------------------------------------
 
 
 
 //VIEW--------------------------------------------------------------------------
 class View {
-public:
-  int initView();
-  void render(Objeto square);
-};
-int View::initView(){
-
+private:
   //Setando Window e Renderer
   SDL_Window* window;
   SDL_Renderer* renderer;
 
-  int posX = 100;
-  int posY = 100;
-  int sizeX = 600;
-  int sizeY = 600;
+public:
+  int initView(int posX, int posY, int sizeX, int sizeY);
+  void render(Objeto square);
+};
+int View::initView(int posX, int posY, int sizeX, int sizeY){
 
 
   //Inicializando SDL
@@ -100,31 +107,105 @@ int View::initView(){
     std::cout << "Erro: " << SDL_GetError();
     return -1;
   }
-  //Renderizando
-  SDL_RenderSetLogicalSize(renderer, sizeX, sizeY);
-  SDL_SetRenderDrawColor(renderer, 255, 0,0,255);
-  SDL_RenderClear(renderer);
-  SDL_RenderPresent(renderer);
-  SDL_Delay(4000);
+  //Setando resolucao
+    SDL_RenderSetLogicalSize(renderer, sizeX, sizeY);
   return 0;
 }
 void View::render(Objeto square){
   SDL_Rect target;
   target.x = square.getPosX();
-  target.y = square.getPosY()+square.getSizeY();
+  target.y = square.getPosY();
+
+  // Carregando texturas
+  // personagem
+  SDL_Texture *texture = IMG_LoadTexture(renderer, square.getSpritePath().c_str());
+  // fundo
+  //SDL_Texture *texture2 = IMG_LoadTexture(renderer, "./park.jpeg");
+
+  // Desenhar a cena
+  SDL_RenderClear(renderer);
+    SDL_SetRenderDrawColor(renderer, 255, 0,0,255);
+  //SDL_RenderCopy(renderer, texture2, nullptr, nullptr);
+  SDL_RenderCopy(renderer, texture, nullptr, &target);
+  SDL_RenderPresent(renderer);
+
+  // Delay para diminuir o framerate
+  SDL_Delay(10);
+
 
 }
 //------------------------------------------------------------------------------
 
 //CONTROLLER--------------------------------------------------------------------
+//this->sprite = IMG_LoadTexture(renderer1, sp.c_str());//endereco da textura
+
+class Controller {
+private:
+
+// Variaveis para verificar eventos
+SDL_Event evento; // eventos discretos
+const Uint8* state = SDL_GetKeyboardState(nullptr); // estado do teclado
+// Controlador:
+bool rodando = true;
+
+public:
+bool getRodando();
+void updateInput();
+const Uint8* getState();
+};
+
+bool Controller::getRodando(){
+  return this->rodando;
+}
+
+void Controller::updateInput(){
+    SDL_PumpEvents(); // atualiza estado do teclado
+    if (evento.type == SDL_QUIT) {
+      rodando = false;
+    }
+}
+
+const Uint8* Controller::getState(){
+  return this->state;
+}
+
 
 //------------------------------------------------------------------------------
 
 
 
 int main(int argc, char* args[]){
+int teste;
+teste = 0;
+Objeto banana(50,50,10,10,10,10,"./banana.png");
+
 View janela;
-janela.initView();
+janela.initView(100,100,600,600);
+
+Controller controle;
+janela.render(banana);
+
+while(teste < 4000){
+teste++;
+controle.updateInput();
+
+if (controle.getState()[SDL_SCANCODE_LEFT]){
+  banana.subPos(1,0);}
+if (controle.getState()[SDL_SCANCODE_RIGHT]) {
+  banana.addPos(1,0);}
+if (controle.getState()[SDL_SCANCODE_UP]) {
+  banana.subPos(0,1);}
+if (controle.getState()[SDL_SCANCODE_DOWN]) {
+  banana.addPos(0,1);}
+
+janela.render(banana);
+}
+
+
+SDL_Quit();
+
+return 0;
+
 }
 
 
